a0fd247d230a97fff655f44bf4a8259a18456fbe
VurDakk <mironov_dm@mail.ru>
2016-09-21 22:06:54 +0500

    Меню пользователя

--- blog/protected/config/main.php
+++ blog/protected/config/main.php
@@ -7,55 +7,52 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Yii Blog Demo',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
 
 	// autoloading model and component classes
-    'import'=>array(
-        'application.models.*',
-        'application.components.*',
-    ),
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
 
-    'modules'=>array(
-        'gii'=>array(
-            'class'=>'system.gii.GiiModule',
-            'password'=>'windows',
-        ),
-    ),
+	'defaultController'=>'post',
 
 	// application components
 	'components'=>array(
-
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-
-		// uncomment the following to enable URLs in path-format
+		'db'=>array(
+			'connectionString' => 'sqlite:protected/data/blog.db',
+			'tablePrefix' => 'tbl_',
+		),
+		// uncomment the following to use a MySQL database
 		/*
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=blog',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+			'tablePrefix' => 'tbl_',
+		),
+		*/
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'post/<id:\d+>/<title:.*?>'=>'post/view',
+				'posts/<tag:.*?>'=>'post/index',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-
-		// database settings are configured in database.php
-        'db'=>array(
-            'connectionString'=>'sqlite:protected/data/blog.db',
-            'tablePrefix'=>'tbl_',
-        ),
-
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>YII_DEBUG ? null : 'site/error',
-		),
-
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
@@ -71,13 +68,9 @@ return array(
 				*/
 			),
 		),
-
 	),
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
+	'params'=>require(dirname(__FILE__).'/params.php'),
 );
